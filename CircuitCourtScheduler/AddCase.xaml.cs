926d765ca14801a147746ed5053999e476501921
gabedeaton <gabedeaton@gmail.com>
2017-02-16 14:06:25 -0500

    added .mdf file

--- CircuitCourtScheduler/AddCase.xaml.cs
+++ CircuitCourtScheduler/AddCase.xaml.cs
@@ -30,7 +30,7 @@ namespace CircuitCourtScheduler
         private void buttonAddCase_Click(object sender, RoutedEventArgs e)
         {
             try {
-                SqlConnection thisConnection = new SqlConnection(@"Server=(localhost);Database=circuitCourtDB;Trusted_Connection=Yes;");
+                SqlConnection thisConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Gabe\Source\Repos\circuitCourtPD\CircuitCourtScheduler\CircuitCourtDB.mdf;Integrated Security=True");
                 thisConnection.Open();
                 string qry = "INSERT into StaffTable [FIRSTNAME,LASTNAME,EMAIL,DATEADDED] values [Johnny,Cash,Jcash@Jmoney.com,@s]";
                 SqlCommand cmd = new SqlCommand(qry);
