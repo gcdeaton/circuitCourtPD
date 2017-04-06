4c14ceebfbca9bb76ee525e01405470843e53005
gabedeaton <gabedeaton@gmail.com>
2017-04-06 19:14:52 -0400

    all combo boxes load data dynamically

--- CircuitCourtScheduler/AddCase.xaml.cs
+++ CircuitCourtScheduler/AddCase.xaml.cs
@@ -15,6 +15,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
 using System.Net.Mail;
+using DatabaseQueries;
 
 namespace CircuitCourtScheduler
 {
@@ -26,10 +27,35 @@ namespace CircuitCourtScheduler
         public Add_Case()
         {
             InitializeComponent();
+            Queries defenderQuery = new Queries(new System.Data.SqlClient.SqlConnection("Data Source=GABE-PC\\SQLEXPRESS;Initial Catalog=PublicDefenders;Integrated Security=True"));
+            defenderQuery.SetSqlCommand("SELECT FIRSTNAME,LASTNAME,EMAIL FROM DefendersTable");
+            defenderQuery.Connect();
+            DataTable dt = defenderQuery.RunSelectQuery();
+            defenderQuery.Disconnect();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                ComboBoxItem cbi = new ComboBoxItem();
+                cbi.Content = row["FIRSTNAME"].ToString() + " " + row["LASTNAME"].ToString();
+                comboBoxDefenders.Items.Add(cbi);
+            }
         }
 
         private void buttonSubmit_Click(object sender, RoutedEventArgs e)
         {
+            LoginClass.email = "emailHere";
+
+            Queries query = new Queries(new System.Data.SqlClient.SqlConnection("Data Source=GABE-PC\\SQLEXPRESS;Initial Catalog=PublicDefenders;Integrated Security=True"));
+            query.SetSqlCommand("INSERT INTO CaseTable(CASENUMBER,USERNAME,DATEOF,CASETYPE,LITIGANT,DEFENDER) VALUES(@CaseNumber,@UserName, @DateOf, @CaseType, @Litigant, @Defender)");
+            query.AddQueryParameters("@CaseNumber", textBoxCaseNumber.Text);
+            query.AddQueryParameters("@UserName", LoginClass.email);
+            query.AddQueryParameters("@DateOf", datePickerHearing.SelectedDate);
+            query.AddQueryParameters("@CaseType", "JD");
+            query.AddQueryParameters("@Litigant", textBoxLitigant.Text);
+            query.AddQueryParameters("@Defender", comboBoxDefenders.SelectedValue);
+            query.Connect();
+            query.ExecuteNonQuery();
+            query.Disconnect();
             sendEmail();
         }
 
--- CircuitCourtScheduler/addDefender.xaml.cs
+++ CircuitCourtScheduler/addDefender.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DatabaseQueries;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,21 +24,35 @@ namespace CircuitCourtScheduler
         public addDefender()
         {
             InitializeComponent();
+
+            Queries query = new Queries(new System.Data.SqlClient.SqlConnection("Data Source=GABE-PC\\SQLEXPRESS;Initial Catalog=PublicDefenders;Integrated Security=True"));
+            query.SetSqlCommand("SELECT NAME FROM LawOfficeTable");
+            query.Connect();
+            DataTable dt = query.RunSelectQuery();
+            query.Disconnect();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                ComboBoxItem cbi = new ComboBoxItem();
+                cbi.Content = row["NAME"];
+                comboBoxLawOffices.Items.Add(cbi);
+            }
         }
 
         private void buttonAddDefender_Click(object sender, RoutedEventArgs e)
         {
             Queries query = new Queries(new System.Data.SqlClient.SqlConnection("Data Source=GABE-PC\\SQLEXPRESS;Initial Catalog=PublicDefenders;Integrated Security=True"));
-            query.SetSqlCommand("INSERT INTO DefenderTable(FIRSTNAME,LASTNAME,LAWOFFICE,EMAIL,CONTACT,ACTIVATED) VALUES(@FirstName,@LastName, @LawOffice, @Email, @Contact, @Activated)");
+            query.SetSqlCommand("INSERT INTO DefendersTable(FIRSTNAME,LASTNAME,LAWOFFICE,EMAIL,CONTACT,ACTIVATED) VALUES(@FirstName,@LastName, @LawOffice, @Email, @Contact, @Activated)");
             query.AddQueryParameters("@FirstName", textBoxFirstName.Text);
-            query.AddQueryParameters("@LastName", textBoxLastName);
-            query.AddQueryParameters("@LawOffice", comboBoxLawOffices.SelectedItem);
+            query.AddQueryParameters("@LastName", textBoxLastName.Text);
+            query.AddQueryParameters("@LawOffice", comboBoxLawOffices.SelectedItem.ToString());
             query.AddQueryParameters("@Email", textBoxEmail.Text);
             query.AddQueryParameters("@Contact", textBoxPhoneNumber.Text);
             query.AddQueryParameters("@Activated", true);
             query.Connect();
             query.ExecuteNonQuery();
             query.Disconnect();
+            this.Close();
         }
 
         private void buttonCancel_Click(object sender, RoutedEventArgs e)
